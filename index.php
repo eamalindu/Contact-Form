decdc6e3380c58db234899ac4086a53086ab17d6
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-09-24 23:01:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <!--bootstrap CSS CDN-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!--custom css-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--boostrap JS CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <!-- jquery CDN-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/07941f149f.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="d-flex justify-content-center align-items-center">
+    <div class="bg-white container w-90 card card-body p-4">
+        <div class="row">
+            <div class="col-lg-4 col-12">
+                <div class="container-fluid h-100 bg-purple text-dark p-3 rounded-2">
+                    <h3 class="text-white fw-semibold mb-4">Contact Us</h3>
+                    <div class="container-fluid mt-4">
+                        <div class="h-25 p-4">
+                            <div class="row">
+                                <div class="col-3 d-flex justify-content-center align-items-center">
+                                    <i class="fa fa-map-marker-alt fa-2x"></i>
+                                </div>
+                                <div class="col-9 d-flex align-items-center justify-content-center">
+                                    <p class="text-one">No 28/F, Gampaha</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="h-25 p-4">
+                            <div class="row">
+                                <div class="col-3 d-flex justify-content-center align-items-center">
+                                    <i class="fa fa-phone fa-2x"></i>
+                                </div>
+                                <div class="col-9 d-flex align-items-center justify-content-center">
+                                    <p class="text-one text-start">033 - 2296132</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="h-25 p-4">
+                            <div class="row">
+                                <div class="col-3 d-flex justify-content-center align-items-center">
+                                    <i class="fa fa-user fa-2x"></i>
+                                </div>
+                                <div class="col-9 d-flex align-items-center justify-content-center">
+                                    <p class="text-one text-wrap">info@gmail.com</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-12 mt-lg-0 mt-3">
+                <h3 class="fw-semibold">Get In Touch</h3>
+                <p>Would you like to get in touch ? We’d be happy to hear from you!</p>
+                <div class="container-fluid p-0">
+                    <input class="custom-input mb-2" type="text" placeholder="Your Name" />
+                    <input class="custom-input mb-2" type="email" placeholder="Your Email Address" />
+                    <input class="custom-input mb-2" type="text" placeholder="Your Subject" />
+                    <input class="custom-input mb-2 double-height" type="text" placeholder="Your Message" />
+                    <button class="btn btn-primary mt-1 float-end">Send Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
